fdb9b86aafe73f70d72e1f8cd1c24ca237689761
Ivan Reshetar <ivan.reshetar@flybits.com>
2019-12-03 17:22:03 -0500

    Update with context

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
-import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
+import React, {useContext} from 'react';
 import './collection.styles.scss';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionsContext from "../../context/collections/collections.context";
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
+
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
@@ -21,8 +20,5 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
 
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
